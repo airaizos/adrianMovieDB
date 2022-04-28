d5d850175bdc0e40e0255d3ee1c0f3989982f1c5
adrian <adrian@iraizos.me>
2022-04-28 12:23:12 +0200

    Initial Commit

---
+++ adrianMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adrianMovieDB
+//
+//  Created by Adrian Iraizos Mendoza on 28/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
