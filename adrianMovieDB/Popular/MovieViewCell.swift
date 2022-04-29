f81df7c6de821e974cb13fbeb393591b76449ecf
adrian <adrian@iraizos.me>
2022-04-29 11:58:58 +0200

    viewCell

---
+++ adrianMovieDB/Popular/MovieViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieViewCell.swift
+//  adrianMovieDB
+//
+//  Created by Adrian Iraizos Mendoza on 29/4/22.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
